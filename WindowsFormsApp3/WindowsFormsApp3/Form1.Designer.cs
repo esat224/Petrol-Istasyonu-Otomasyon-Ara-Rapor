6592bf03e223b8acd327ba2f9a07ba47fee6ffec
esat224 <166159979+esat224@users.noreply.github.com>
2024-12-09 16:22:44 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.workers = new System.Windows.Forms.ListBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // workers
+            // 
+            this.workers.FormattingEnabled = true;
+            this.workers.Location = new System.Drawing.Point(45, 36);
+            this.workers.Name = "workers";
+            this.workers.Size = new System.Drawing.Size(343, 225);
+            this.workers.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(45, 291);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "button1";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.workers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox workers;
+        private System.Windows.Forms.Button btnLoadData;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        // Bağlantı dizesi
+        private string connectionString = "Server=127.0.0.1;Database=petrol;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+
+            // ListBox'ı temizle
+            workers.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT NAME , SURNAME , WAGE  FROM workers";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // name ve surname değerlerini birleştirip ListBox'a ekle
+                        string fullName = $"{reader["NAME"]} {reader["SURNAME"]}";
+                        workers.Items.Add(fullName);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+    }
+}
